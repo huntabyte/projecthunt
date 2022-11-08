8db4c34e9be050341dd560ccd0521b0d4f75cb8b
Hunter Johnston <johnstonhuntera@gmail.com>
2022-11-07 22:33:48 -0500

    add redirect to project on project creation

--- apps/web/src/routes/projects/new/+page.server.ts
+++ apps/web/src/routes/projects/new/+page.server.ts
@@ -1,15 +1,16 @@
 import { serialize } from 'object-to-formdata';
-import { error, invalid } from '@sveltejs/kit';
+import { error, invalid, redirect } from '@sveltejs/kit';
 import type { Actions } from './$types';
 import type { ClientResponseError } from 'pocketbase';
 import { validateData } from '$lib/helpers';
 import { createProjectDto } from '$lib/schemas';
+import type { Project } from '$lib/types';
 
 export const actions: Actions = {
 	create: async ({ request, locals }) => {
 		const { formData, errors } = await validateData(request, createProjectDto);
 		console.log(typeof formData.thumbnail);
-
+		let projectId;
 		if (errors) {
 			const { thumbnail, ...rest } = formData;
 			return invalid(400, {
@@ -18,11 +19,14 @@ export const actions: Actions = {
 			});
 		}
 		try {
-			await locals.pb.collection('projects').create(serialize(formData));
+			const { id } = await locals.pb.collection('projects').create<Project>(serialize(formData));
+			projectId = id;
 		} catch (err) {
 			console.log('Error:', err);
 			const e = err as ClientResponseError;
 			throw error(e.status, e.data.message);
 		}
+
+		throw redirect(303, `/projects/${projectId}`);
 	}
 };
